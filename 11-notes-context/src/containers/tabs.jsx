fe416711e7c053c27bb845c72b1b8785f20314ef
Hawa BahBah <hawabahbah25@gmail.com>
2021-05-28 11:31:38 +0100

    adding notes-lesson

--- 11-notes-context/src/containers/tabs.jsx
+++ 11-notes-context/src/containers/tabs.jsx
@@ -1,19 +1,16 @@
-import { connect } from 'react-redux';
-import Tabs from '../components/tabs/tabs.jsx';
-import { setActiveTab } from '../redux/actions/tabs.js';
-import { getTabs, getActiveTabId } from '../redux/selectors/tabs.js';
-
+import { connect } from "react-redux";
+import Tabs from "../components/tabs/tabs.jsx";
+import { setActiveTab } from "../redux/actions/tabs.js";
+//selectors:
+import { getTabs, getActiveTabId } from "../redux/selectors/tabs.js";
 
 const mapStateToProps = (state) => ({
-    tabs: getTabs(state),
-    activeTabId: getActiveTabId(state),
+  activeTabId: getActiveTabId(state), // now if 'setActiveTab(state)' is called again it will return the CACHED value
+  tabs: getTabs(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    onSetActiveTab: (id) => () => dispatch(setActiveTab(id)),
+  onSetActiveTab: (id) => () => dispatch(setActiveTab(id)),
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Tabs);
+export default connect(mapStateToProps, mapDispatchToProps)(Tabs);
--- 11-notes-context/src/redux/selectors/tabs.js
+++ 11-notes-context/src/redux/selectors/tabs.js
@@ -1,14 +1,19 @@
-import { createSelector } from 'reselect';
+import { createSelector } from "reselect";
 
+// the selector is a function that returns the relevant state
+export const getTabs = (state) => state.tabs;
 
-export const getTabs = state => state.tabs;
-
+//another seletctor function
 export const getActiveTab = createSelector(
-    getTabs,
-    tabs => tabs.find(tab => tab.isActive),
+  // 1st argument: get tabs state
+  getTabs,
+  // 2nd argument: actual function
+  // this tabs comes from the previous argument
+  (tabs) => tabs.find((tab) => tab.isActive)
 );
 
+//selector:
 export const getActiveTabId = createSelector(
-    getActiveTab,
-    activeTab => activeTab.id,
+  getActiveTab,
+  (activeTab) => activeTab.id
 );
